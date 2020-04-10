9fdadbdd09131cad4b7b5c66cf653c4386da1ad3
Азиз Исматов <yourGitHub@example.co>
2020-04-10 21:49:44 +0500

    Создал 2 интерфейс с методами

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+namespace myNameSpace
+{
+  class Programm
+ { 
+   static void Main()
+  {
+   
+
+  }
+
+ }
+  interface IPlayable
+  {
+      void play()
+      {
+
+      }
+      void pause()
+      {
+
+      }
+      void stop()
+      {
+        
+      }
+    }  
+    interface IRecodable
+    {
+         void Record()
+        {
+
+        }
+        void Pause() 
+        {
+
+        }
+        void Stop()
+        {
+
+        }
+    }
+   
+      
+    
+}
